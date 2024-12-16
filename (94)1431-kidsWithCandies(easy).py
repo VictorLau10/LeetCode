1fea976198d992faef1990f272d27f6493738e29
victor <115098603+VictorLau10@users.noreply.github.com>
2024-12-16 14:08:13 -0500

    Sep 22, 2024 (Beats 5.29% Runtime, Beats 33.94% Memory)

---
+++ (94)1431-kidsWithCandies(easy).py
@@ -0,0 +1,21 @@
+# 1431. Kids With the Greatest Number of Candies
+# Sep 22, 2024
+# Beats 5.29% Runtime
+# Beats 33.94% Memory
+
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        greatest = max(candies)
+        lst = []
+        for kid in candies:
+            if kid + extraCandies >= greatest:
+                lst.append(True)
+            else: 
+                lst.append(False)
+        return lst
+        
